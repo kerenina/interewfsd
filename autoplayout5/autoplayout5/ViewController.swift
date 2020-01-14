7897815cc843a35ceed79b71cf6473dbcf3884a8
kerenina <42427334+kerenina@users.noreply.github.com>
2020-01-14 12:10:45 -0600

    Add files via upload

---
+++ autoplayout5/autoplayout5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoplayout5
+//
+//  Created by 2020-1 on 14/01/20.
+//  Copyright © 2020 adanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
